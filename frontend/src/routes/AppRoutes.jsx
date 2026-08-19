9e84f5bade449bd33187b04a9681e68466f8f4f4
Pratulshit <pratulshit9907@gmail.com>
2026-08-20 00:51:57 +0530

    Fix root route redirect to login

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/auth/Login";
-
 
 function AppRoutes() {
     return (
         <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
             <Route path="/login" element={<Login />} />
         </Routes>
     );
